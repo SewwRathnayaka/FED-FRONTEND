ae71276a7eafda2a0b936b1f37e1a35cff07039f
Sewwandi <22ug1-0805@sltc.ac.lk>
2025-02-22 16:39:34 +0530

    Connecting Backend on frontend

--- src/lib/api.js
+++ src/lib/api.js
@@ -5,7 +5,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const Api = createApi({
   reducerPath: "Api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://fed-storefront-backend-sewwandi.onrender.com/api/",
+    baseUrl: "/api/",
     prepareHeaders: async (headers, { getState }) => {
       const token = await window.Clerk?.session?.getToken();
       if (token) {
